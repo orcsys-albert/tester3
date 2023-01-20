3f9bdc0f7bac5f429d92d90c3c60871acd6073b1
Albert Angmor <albert@treepz.com>
2023-01-20 14:56:11 +0000

    v4

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <title>Tester3 Simple Page</title>
 </head>
 <body>
-<h3>CI/CD Testing with Teamcity On-premise v1.3</h3>
+<h3>CI/CD Testing with Teamcity On-premise v1.4</h3>
 <br>
 <iframe src="http://192.168.15.13/externalStatus.html"/>
 </body>
